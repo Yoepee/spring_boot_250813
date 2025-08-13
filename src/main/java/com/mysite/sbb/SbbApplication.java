478b6beb621e6680828413db5207cbc336c7e6df
Yoepee <kdy2975@naver.com>
2025-08-13 15:51:28 +0900

    출력 테스트

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot250813Application {
+public class SbbApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot250813Application.class, args);
+        SpringApplication.run(SbbApplication.class, args);
     }
 
 }
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBoot250813ApplicationTests {
+class SbbApplicationTests {
 
     @Test
     void contextLoads() {
