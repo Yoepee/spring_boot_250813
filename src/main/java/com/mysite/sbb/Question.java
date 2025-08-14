0bc9a3f3d79a0adb14b0239e52539d2159afcc90
Yoepee <kdy2975@naver.com>
2025-08-14 14:20:40 +0900

    샘플 데이터 생성, 테스트 변경

--- src/main/java/com/mysite/sbb/Question.java
+++ src/main/java/com/mysite/sbb/Question.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -21,7 +21,7 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
 
-    private LocalDate createDate;
+    private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<Answer> answerList;
--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,15 +1,33 @@
 package com.mysite.sbb;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ActiveProfiles("test")
 @SpringBootTest
 class SbbApplicationTests {
+    @Autowired
+    private QuestionRepository questionRepository;
 
     @Test
-    void contextLoads() {
+    @DisplayName("findAll")
+    void t1() {
+        List<Question> questionList = questionRepository.findAll();
+
+        assertThat(questionList).size().isEqualTo(2);
+        assertEquals(2, questionList.size());
+
+        Question q1 = questionList.get(0);
+        assertThat(q1.getSubject()).isEqualTo("sbb가 무엇인가요?");
+        assertEquals("sbb가 무엇인가요?", q1.getSubject());
     }
 
 }
