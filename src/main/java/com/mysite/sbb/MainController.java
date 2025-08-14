85e0d55ab4a1fdc5248af333aa028ec72b6f3be8
Yoepee <kdy2975@naver.com>
2025-08-14 09:48:32 +0900

    ResponseBody 추가

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,16 +2,19 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @ResponseBody
     @GetMapping("/")
-    public void index() {
-        System.out.println("index 메소드 called");
+    public String index() {
+        return "sbb";
     }
 
+    @ResponseBody
     @GetMapping("/hello")
-    public void hello() {
-        System.out.println("hello 메소드 called");
+    public String hello() {
+        return "안녕하세요";
     }
 }
