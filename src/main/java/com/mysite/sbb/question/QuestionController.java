bcd00e8d2b1936c2688403d3cfed3a661fce6472
Yoepee <kdy2975@naver.com>
2025-08-20 09:31:22 +0900

    질문 등록 DB 연동

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -3,9 +3,7 @@ package com.mysite.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +33,12 @@ public class QuestionController {
     @GetMapping("/create")
     public String create(Model model) {
         return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String createQuestion(@RequestParam("subject") String subject, @RequestParam("content") String content) {
+        Question question = questionService.create(subject, content);
+
+        return "redirect:/question/detail/%d".formatted(question.getId());
     }
 }
